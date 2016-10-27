d2469f5487dd750904abdf3962e49ca8ffec4c8d
urielsalis <uriel@urielsalis.me>
2016-10-27 01:46:15 -0300

    happy?

--- src/main/java/me/urielsalis/urielsalads/extensions/intelDownload/config/Intel.java
+++ src/main/java/me/urielsalis/urielsalads/extensions/intelDownload/config/Intel.java
@@ -1,28 +1,42 @@
 package me.urielsalis.urielsalads.extensions.intelDownload.config;
 
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
- * Created by cecis on 27/10/2016.
+ * UrielSalads
+ * Copyright (C) 2016 Uriel Salischiker
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class Intel {
     String version;
     Date lastPartialUpdate;
     Date lastFullUpdate;
     String name;
-    ArrayList<Driver> drivers;
-    ArrayList<Ark> families;
+    List<Driver> drivers;
+    List<Ark> families;
 
     class Driver {
         String name;
         int epmID;
-        ArrayList<Download> downloads;
+       List<Download> downloads;
     }
-    
+
     class Ark {
         String name;
-        ArrayList<Download> download;
+        List<Download> download;
     }
 
 }
--- src/main/java/me/urielsalis/urielsalads/extensions/intelDownload/config/OS.java
+++ src/main/java/me/urielsalis/urielsalads/extensions/intelDownload/config/OS.java
@@ -1,7 +1,21 @@
 package me.urielsalis.urielsalads.extensions.intelDownload.config;
 
 /**
- * Created by cecis on 27/10/2016.
+ * UrielSalads
+ * Copyright (C) 2016 Uriel Salischiker
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class OS {
     String name;
